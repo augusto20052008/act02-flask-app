6078f5d21b2bd768e5ec3a7792eb5167dc1fdbb3
augusto20052008 <augustovdavilar@gmail.com>
2025-04-16 11:15:36 -0500

    Fix IndexError en home

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
 from flask import Flask
-from datetime import datetime
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Leer archivo desde URL con requests
+    # URL del archivo remoto
     url = "https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt"
     response = requests.get(url)
-    contenido = response.text.splitlines()  # Lista de líneas del archivo
+    contenido = response.text
 
-    # Filtrar las personas cuyo ID empieza con 3, 4, 5 o 7
-    ids_validos = ('3', '4', '5', '7')
-    personas = [linea.split(';') for linea in contenido if linea.startswith(ids_validos)]
+    tabla_html = "<table border='1'>"
+    tabla_html += "<tr><th>ID</th><th>Nombre</th><th>Apellido</th><th>Email</th></tr>"
 
-    # Construir tabla HTML
-    tabla_html = """
-    <h2>Personas filtradas (ID empieza con 3, 4, 5 o 7)</h2>
-    <table border="1" cellpadding="5" cellspacing="0">
-        <tr><th>ID</th><th>Nombre</th><th>Apellido</th><th>Correo</th></tr>
-    """
-
-    for persona in personas:
-        tabla_html += f"<tr><td>{persona[0]}</td><td>{persona[1]}</td><td>{persona[2]}</td><td>{persona[3]}</td></tr>"
+    lineas = contenido.strip().split('\n')
+    for linea in lineas:
+        persona = linea.strip().split(',')
+        
+        # Verificamos que tenga al menos 4 campos
+        if len(persona) >= 4:
+            id_persona = persona[0]
+            if id_persona.startswith(('3', '4', '5', '7')):
+                tabla_html += f"<tr><td>{persona[0]}</td><td>{persona[1]}</td><td>{persona[2]}</td><td>{persona[3]}</td></tr>"
 
     tabla_html += "</table>"
 
