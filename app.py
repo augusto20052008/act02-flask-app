862ccf33ca13dc0f3cc6e6ec75ccb48c85a95ef7
augusto20052008 <augustovdavilar@gmail.com>
2025-04-16 11:23:38 -0500

    ID que inician en 3, 4, 5, 7

--- app.py
+++ app.py
@@ -5,26 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # URL del archivo remoto
     url = "https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt"
     response = requests.get(url)
     contenido = response.text
 
-    tabla_html = "<table border='1'>"
+    tabla_html = "<h1>Personas con ID iniciando en 3, 4, 5 o 7</h1>"
+    tabla_html += "<table border='1' cellpadding='5' cellspacing='0'>"
     tabla_html += "<tr><th>ID</th><th>Nombre</th><th>Apellido</th><th>Email</th></tr>"
 
     lineas = contenido.strip().split('\n')
     for linea in lineas:
         persona = linea.strip().split(',')
         
-        # Verificamos que tenga al menos 4 campos
         if len(persona) >= 4:
             id_persona = persona[0]
             if id_persona.startswith(('3', '4', '5', '7')):
                 tabla_html += f"<tr><td>{persona[0]}</td><td>{persona[1]}</td><td>{persona[2]}</td><td>{persona[3]}</td></tr>"
 
     tabla_html += "</table>"
-
     return tabla_html
 
 if __name__ == '__main__':
